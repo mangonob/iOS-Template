ce4a4f645a20b604de8fb2cdd186eafe8b0634e7
mangonob <mangonob@qq.com>
2018-02-01 17:59:21 +0800

    Initial Commit

---
+++ iOS-Template/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iOS-Template
+//
+//  Created by 高炼 on 2018/2/1.
+//  Copyright © 2018年 Trinity. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
